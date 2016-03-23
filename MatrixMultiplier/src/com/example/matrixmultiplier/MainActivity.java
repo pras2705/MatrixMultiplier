2c96563612a050c71198f29ab1cda1deb7bf76f5
pras2705 <prasolution@gmail.com>
2016-03-23 21:26:25 +0530

    adding files

---
+++ MatrixMultiplier/src/com/example/matrixmultiplier/MainActivity.java
@@ -0,0 +1,168 @@
+package com.example.matrixmultiplier;
+
+
+import java.util.Map;
+
+import com.inmobi.ads.InMobiAdRequestStatus;
+import com.inmobi.ads.InMobiBanner;
+import com.inmobi.sdk.InMobiSdk;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends Activity {
+
+	
+	EditText num1,num2,num3,num4,num5,num6,num7,num8,num9,num10,num11,num12,num13,num14,num15,num16,num17,num18;
+	
+	int firstArray[][] = new int[3][3];
+	int secondArray[][] = new int[3][3];
+	int productArray[][] = new int[3][3];
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		//finding the textbox views
+		intializeNumbers();
+		InMobiSdk.init(this, "4028cba632f2db85013300695de90166");
+		InMobiBanner imbanner = new InMobiBanner(this, 1458967564949L);
+		//finding the layout view for the BannerAd
+		RelativeLayout rl = (RelativeLayout) findViewById(R.id.ad_container);
+		imbanner.setListener(new InMobiBanner.BannerAdListener() {
+             @Override
+             public void onAdLoadSucceeded(InMobiBanner inMobiBanner) {
+             }
+
+             @Override
+             public void onAdLoadFailed(InMobiBanner inMobiBanner,
+                                        InMobiAdRequestStatus inMobiAdRequestStatus) {
+             }
+
+             @Override
+             public void onAdDisplayed(InMobiBanner inMobiBanner) {
+             }
+
+             @Override
+             public void onAdDismissed(InMobiBanner inMobiBanner) {
+             }
+
+             @Override
+             public void onAdInteraction(InMobiBanner inMobiBanner, Map<Object, Object> map) {
+             }
+
+             @Override
+             public void onUserLeftApplication(InMobiBanner inMobiBanner) {
+             }
+
+             @Override
+             public void onAdRewardActionCompleted(InMobiBanner inMobiBanner, Map<Object, Object> map) {
+             }
+         });
+		//sizing and positioning(customizing) the ad layout
+		 int width = toPixelUnits(320);
+         int height= toPixelUnits(50);
+         RelativeLayout.LayoutParams bannerLayoutParams =
+                 new RelativeLayout.LayoutParams(width, height);
+         bannerLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+         bannerLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+		 rl.addView(imbanner,bannerLayoutParams );
+		 imbanner.load();
+		
+	}
+	
+	
+	/**
+	 * Finds the textbox views
+	 */
+	public void intializeNumbers(){
+		num1 = (EditText) findViewById(R.id.editText1);
+		num2 = (EditText) findViewById(R.id.editText6);
+		num3 = (EditText) findViewById(R.id.editText7);
+		num4 = (EditText) findViewById(R.id.editText4);
+		num5 = (EditText) findViewById(R.id.editText5);
+		num6 = (EditText) findViewById(R.id.editText8);
+		num7 = (EditText) findViewById(R.id.editText9);
+		num8 = (EditText) findViewById(R.id.editText10);
+		num9 = (EditText) findViewById(R.id.editText11);
+		num10 = (EditText) findViewById(R.id.editText2);
+		num11 = (EditText) findViewById(R.id.editText3);
+		num12 = (EditText) findViewById(R.id.editText12);
+		num13 = (EditText) findViewById(R.id.editText13);
+		num14 = (EditText) findViewById(R.id.editText14);
+		num15 = (EditText) findViewById(R.id.editText15);
+		num16 = (EditText) findViewById(R.id.editText16);
+		num17 = (EditText) findViewById(R.id.editText17);
+		num18 = (EditText) findViewById(R.id.editText18);
+	}
+	
+
+	/**
+	 * Submit button action of passing the solution to SolutionActivity
+	 * @param v
+	 */
+	public void submitSolution(View v){
+		calculateProduct();
+		Intent i = new Intent();
+		i.setClassName("com.example.matrixmultiplier", "com.example.matrixmultiplier.SolutionActivity");
+		i.putExtra("1stRow", productArray[0]);
+		i.putExtra("2ndRow", productArray[1]);
+		i.putExtra("3rdRow", productArray[2]);
+		startActivity(i);
+	}
+	
+	/**
+	 * 3 X 3 Matrix Multiplication
+	 */
+	public void calculateProduct(){
+		getNumValuesandBuildMatrices();
+		int c,d,k,sum = 0;
+		 for ( c = 0 ; c < 3 ; c++ )
+         {
+            for ( d = 0 ; d < 3 ; d++ )
+            {   
+               for ( k = 0 ; k < 3 ; k++ )
+               {
+                  sum = sum + firstArray[c][k]*secondArray[k][d];
+               }
+               productArray[c][d] = sum;
+               sum = 0;
+            }
+         }
+	}
+	
+	/**
+	 * Building the individual matrices based on the inputs.
+	 */
+	public void getNumValuesandBuildMatrices(){
+		firstArray[0][0] =	(!num1.getText().toString().equals(""))?  Integer.parseInt(num1.getText().toString()): 0;
+		firstArray[0][1] = (!num2.getText().toString().equals(""))?  Integer.parseInt(num2.getText().toString()):0;
+		firstArray[0][2] = (!num3.getText().toString().equals(""))? Integer.parseInt(num3.getText().toString()):0;
+		firstArray[1][0] = (!num4.getText().toString().equals(""))? Integer.parseInt(num4.getText().toString()):0;
+		firstArray[1][1] =(!num5.getText().toString().equals(""))?  Integer.parseInt(num5.getText().toString()):0;
+		firstArray[1][2] = (!num6.getText().toString().equals(""))? Integer.parseInt(num6.getText().toString()):0;
+		firstArray[2][0] = (!num7.getText().toString().equals(""))? Integer.parseInt(num7.getText().toString()):0;
+		firstArray[2][1] = (!num8.getText().toString().equals(""))? Integer.parseInt(num8.getText().toString()):0;
+		firstArray[2][2] = (!num9.getText().toString().equals(""))? Integer.parseInt(num9.getText().toString()):0;
+		secondArray[0][0] = (!num10.getText().toString().equals(""))? Integer.parseInt(num10.getText().toString()):0;
+		secondArray[0][1] =(!num11.getText().toString().equals(""))?  Integer.parseInt(num11.getText().toString()):0;
+		secondArray[0][2] = (!num12.getText().toString().equals(""))? Integer.parseInt(num12.getText().toString()):0;
+		secondArray[1][0] = (!num13.getText().toString().equals(""))? Integer.parseInt(num13.getText().toString()):0;
+		secondArray[1][1] = (!num14.getText().toString().equals(""))? Integer.parseInt(num14.getText().toString()):0;
+		secondArray[1][2] = (!num15.getText().toString().equals(""))? Integer.parseInt(num15.getText().toString()):0;
+		secondArray[2][0] = (!num16.getText().toString().equals(""))? Integer.parseInt(num16.getText().toString()):0;
+		secondArray[2][1] =(!num17.getText().toString().equals(""))?  Integer.parseInt(num17.getText().toString()):0;
+		secondArray[2][2] =(!num18.getText() .toString().equals(""))?  Integer.parseInt(num18.getText().toString()):0;
+	}
+	
+	private int toPixelUnits(int dipUnit) {
+        float density = getResources().getDisplayMetrics().density;
+        return Math.round(dipUnit * density);
+    }
+}
---
+++ MatrixMultiplier/src/com/example/matrixmultiplier/SolutionActivity.java
@@ -0,0 +1,151 @@
+package com.example.matrixmultiplier;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import com.inmobi.ads.InMobiAdRequestStatus;
+import com.inmobi.ads.InMobiBanner;
+import com.inmobi.ads.InMobiInterstitial;
+import com.inmobi.sdk.InMobiSdk;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class SolutionActivity extends Activity {
+    
+	EditText num1,num2,num3,num4,num5,num6,num7,num8,num9;
+	int arr1[],arr2[],arr3[];
+	InMobiInterstitial interstitial;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_solution);
+		//getting the intent values sent from MainActivity
+		arr1 = getIntent().getIntArrayExtra("1stRow");
+		arr2 = getIntent().getIntArrayExtra("2ndRow");
+		arr3 = getIntent().getIntArrayExtra("3rdRow");
+		initializeElements();
+		setArrayElements();
+		InMobiSdk.init(this, "4028cba632f2db85013300695de90166");
+		InMobiBanner imbanner = new InMobiBanner(this, 1458967564949L);
+		
+		RelativeLayout rl = (RelativeLayout) findViewById(R.id.sol_ad_container);
+		imbanner.setListener(new InMobiBanner.BannerAdListener() {
+             @Override
+             public void onAdLoadSucceeded(InMobiBanner inMobiBanner) {
+             }
+
+             @Override
+             public void onAdLoadFailed(InMobiBanner inMobiBanner,
+                                        InMobiAdRequestStatus inMobiAdRequestStatus) {
+             }
+
+             @Override
+             public void onAdDisplayed(InMobiBanner inMobiBanner) {
+             }
+
+             @Override
+             public void onAdDismissed(InMobiBanner inMobiBanner) {
+             }
+
+             @Override
+             public void onAdInteraction(InMobiBanner inMobiBanner, Map<Object, Object> map) {
+             }
+
+             @Override
+             public void onUserLeftApplication(InMobiBanner inMobiBanner) {
+             }
+
+             @Override
+             public void onAdRewardActionCompleted(InMobiBanner inMobiBanner, Map<Object, Object> map) {
+             }
+         });
+		
+		 int width = toPixelUnits(320);
+         int height= toPixelUnits(50);
+         RelativeLayout.LayoutParams bannerLayoutParams =
+                 new RelativeLayout.LayoutParams(width, height);
+         bannerLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+         bannerLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+		 rl.addView(imbanner,bannerLayoutParams );
+		imbanner.load();
+		
+		
+		InMobiInterstitial.InterstitialAdListener interstitialAdListener = new InMobiInterstitial.InterstitialAdListener() {
+			@Override
+			public void onAdLoadSucceeded(InMobiInterstitial ad) {
+			if(ad.isReady()){
+			ad.show();
+			}
+			}
+			@Override
+			public void onAdLoadFailed(InMobiInterstitial ad, InMobiAdRequestStatus requestStatus) {}
+			@Override
+			public void onAdDisplayed(InMobiInterstitial ad) {}
+			@Override
+			public void onAdDismissed(InMobiInterstitial ad) {}
+			@Override
+			public void onAdInteraction(InMobiInterstitial ad, Map<Object, Object> params) {}
+			@Override
+			public void onAdRewardActionCompleted(InMobiInterstitial ad, Map<Object, Object> rewards) {}
+			@Override
+			public void onUserLeftApplication(InMobiInterstitial ad) {}
+			};
+		 interstitial = new InMobiInterstitial(this, 1459827822090L,interstitialAdListener );
+		
+	}
+
+	
+   /**
+ * finding the textBox Views
+ */
+private void initializeElements(){
+	num1 = (EditText) findViewById(R.id.solEditText1);
+	num2 = (EditText) findViewById(R.id.solEditText2);
+	num3 = (EditText) findViewById(R.id.solEditText3);
+	num4 = (EditText) findViewById(R.id.solEditText4);
+	num5 = (EditText) findViewById(R.id.solEditText5);
+	num6 = (EditText) findViewById(R.id.solEditText6);
+	num7 = (EditText) findViewById(R.id.solEditText7);
+	num8 = (EditText) findViewById(R.id.solEditText8);
+	num9 = (EditText) findViewById(R.id.solEditText9);
+    }	
+
+	/**
+	 * Setting the textBox
+	 */
+	private void setArrayElements(){
+		num1.setText(String.valueOf(arr1[0]));
+		num2.setText(String.valueOf(arr1[1]));
+		num3.setText(String.valueOf(arr1[2]));
+		num4.setText(String.valueOf(arr2[0]));
+		num5.setText(String.valueOf(arr2[1]));
+		num6.setText(String.valueOf(arr2[2]));
+		num7.setText(String.valueOf(arr3[0]));
+		num8.setText(String.valueOf(arr3[1]));
+		num9.setText(String.valueOf(arr3[2]));
+		
+	}
+	
+	/**
+	 * InputAgain button action
+	 */
+	public void goToMainActivity(View v){
+		Intent i = new Intent();
+		i.setClassName("com.example.matrixmultiplier", "com.example.matrixmultiplier.MainActivity");
+		interstitial.load();
+		startActivity(i);
+	}
+	
+	private int toPixelUnits(int dipUnit) {
+        float density = getResources().getDisplayMetrics().density;
+        return Math.round(dipUnit * density);
+    }
+}
